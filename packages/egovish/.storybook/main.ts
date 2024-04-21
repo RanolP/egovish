10422c7ed77474e4494ed447ccf6e91797ae0385
RanolP <me@ranolp.dev>
2024-04-22 01:48:55 +0900

    chore: move folder

--- packages/egovish/.storybook/main.ts
+++ packages/egovish/.storybook/main.ts
@@ -1,7 +1,7 @@
 import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../docs/**/*.mdx', '../docs/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
